1b115c1f872fc7b60709f3791b779fc9cf0fd350
Risky Armansyah <91355361+rarmnsyah@users.noreply.github.com>
2023-08-13 21:30:56 +0700

    Update login-script.js

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,12 +1,4 @@
 /** @module Login-Script */
-
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
 /** 
 * Membuat variabel loginFormElement untuk tampilan form.
 * @constant {HTMLElement}
@@ -27,14 +19,37 @@ const inputEmailElement = document.querySelector('#inputEmail');
 */
 const inputPasswordElement = document.querySelector('#inputPassword');
  
-
+ 
+/** 
+ * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
+ * @constant {string}
+ */
+const expectedEmail = 'admin@dicoding.com';
+ 
+ 
+/** 
+ * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
+ * @constant {string}
+ */
+const expectedPassword = 'superpassword'
+ 
+ 
 /* Comment :  Menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
   
-  /* Comment : Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
+  /** 
+   * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
+   * @constant {string}
+   */
   const email = inputEmailElement.value;
+  
+  /** 
+   * Membuat variabel password untuk menyimpan niali password yang didapatkan saat button ditekan.
+   * @constant {string}
+   */
   const password = inputPasswordElement.value;
+ 
  
   /* Comment : Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
   if (email == expectedEmail && password == expectedPassword) {
@@ -47,4 +62,5 @@ loginFormElement.addEventListener('submit', function(event) {
     /* Comment : Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
     showPopUp();
  
+  }
 });
